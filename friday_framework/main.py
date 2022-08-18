25cab281b9d9c0d3695b73917c8b93d6caec0a9e
UegenePalimov <palbimov@mail.ru>
2022-08-18 23:28:06 +0500

    HomeWork_4 checking the operation of the code on the server v.3.4

--- friday_framework/main.py
+++ friday_framework/main.py
@@ -27,7 +27,8 @@ class Framework:
         if method == 'POST':
             data = PostRequests().get_request_params(environ)
             request['data'] = data
-            print(f'We received a post request: {data}.')
+            # print(f'We received a post request: {data}.')
+            print(request['data'])
         if method == 'GET':
             request_params = GetRequests().get_request_params(environ)
             request['request_params'] = request_params
