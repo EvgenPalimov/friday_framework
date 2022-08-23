4e236ca8060aed0a3149a420b9bfce16f8fd59d2
UegenePalimov <palbimov@mail.ru>
2022-08-23 13:39:18 +0500

    HomeWork_4 new version v.18.1

--- staticfiles/js/ajax_method.js
+++ staticfiles/js/ajax_method.js
@@ -19,24 +19,24 @@ window.onload = () => {
         e.preventDefault();
     })
 
-    $('.update_course_ajax').on('click', 'button[type="button"]', (e) => {
-        let t_href = e.target.value;
-        $.ajax({
-            url: '/course-list/',
-            type: 'POST',
-            data: jQuery.param({id: t_href, method: 'detail'}),
-            success: function (response) {
-                console.log(response);
-                $('.update_course').html(response)
-                $('#course_update_id').trigger('click');
-
-            },
-           error: function(error){
-				console.log(error);
-			}
-        });
-        e.preventDefault();
-    })
+    // $('.update_course_ajax').on('click', 'button[type="button"]', (e) => {
+    //     let t_href = e.target.value;
+    //     $.ajax({
+    //         url: '/course-list/',
+    //         type: 'POST',
+    //         data: jQuery.param({id: t_href, method: 'detail'}),
+    //         success: function (response) {
+    //             console.log(response);
+    //             $('.update_course').html(response)
+    //             $('#course_update_id').trigger('click');
+    //
+    //         },
+    //        error: function(error){
+	// 			console.log(error);
+	// 		}
+    //     });
+    //     e.preventDefault();
+    // })
 
 }
 
