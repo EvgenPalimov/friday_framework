5ddcbd1ab32e707c634448d31022f4865e58c75d
UegenePalimov <palbimov@mail.ru>
2022-08-23 14:26:38 +0500

    HomeWork_4 new version v.18.18

--- views.py
+++ views.py
@@ -161,7 +161,7 @@ class Courses:
             logger.log('Updating Training')
             id = int(request['data']['id'])
             name = request['data']['name']
-            type = request['data']['type_course']
+            type = int(request['data']['type_course'])
             result = site.course_update(id,name, type)
             return '200 OK', render('courses.html',
                                     objects_list=result)
